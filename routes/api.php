340e4d53fe90a4767a8e5c0b4e37622e6ce6ea36
Septiancesare <112379597+Septiancesare@users.noreply.github.com>
2024-12-22 23:44:47 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Models\Category;
+use App\Models\District;
 use App\Models\Product;
+use App\Models\Province;
+use App\Models\Regency;
+use App\Models\User;
+use App\Models\Village;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,20 @@ Route::get('/products', function () {
 
 Route::get('/categories', function () {
     return Category::all();
+});
+
+Route::get('/getProvinces', function () {
+    return Province::all();
+});
+
+Route::get('/getRegencies/{province_id}', function ($province_id) {
+    return Regency::where('province_id', $province_id)->get();
+});
+
+Route::get('/getDistricts/{regency_id}', function ($regency_id) {
+    return District::where('regency_id', $regency_id)->get();
+});
+
+Route::get('/getVillages/{district_id}', function ($district_id) {
+    return Village::where('district_id', $district_id)->get();
 });
